6453905a68f19e94b2eff0b880d20a0136b6ac51
Blake Guidry <guidry.bd@gmail.com>
2020-06-04 22:05:27 -0500

    Implemented elapsed, interval, remaining, current, next

--- components/Session/Timer.js
+++ components/Session/Timer.js
@@ -4,8 +4,12 @@ import { StyleSheet, View, StatusBar, TouchableOpacity, Text, Dimensions } from 
 const screen = Dimensions.get('window');
 
 const Timer = props => {
-  const [remaining, setRemaining] = useState(60);
+  const [remaining, setRemaining] = useState(10);
   const [active, setActive] = useState(false);
+  const [elapsed, setElapsed] = useState(0);
+  const [int, setInt] = useState(0);
+  const [remain, setRemain] = useState(60);
+  const [stage, setStage] = useState(1);
 
   const startStop = () => {
     setActive(!active);
@@ -13,7 +17,11 @@ const Timer = props => {
 
   const reset = () => {
     setActive(false);
-    setRemaining(60);
+    setRemaining(10);
+    setElapsed(0);
+    setInt(0);
+    setRemain(60);
+    setStage(1);
   }
 
   useEffect(() => {
@@ -21,6 +29,28 @@ const Timer = props => {
     if (active) {
       interval = setInterval(() => {
         setRemaining(remaining => remaining - 1);
+
+        // Elapsed and remaining for hangs
+        if (stage % 2 === 0 && remaining > 0) {
+          setElapsed(elapsed => elapsed + 1);
+          setRemain(remain => remain - 1);
+        }
+  
+        // Reset time for rests and hangs
+        if (remaining === 0) {
+          if (stage % 2 !== 0) {
+            setInt(int => int + 1);
+            setStage(stage => stage + 1);
+            setRemaining(10);
+          } else if (stage % 2 === 0 && stage < 12) {
+            setStage(stage => stage + 1);
+            setRemaining(5);
+          } else {
+            clearInterval(interval);
+            setRemaining(0);
+          }
+        }
+
       }, 1000);
     } else if (!active) {
       clearInterval(interval);
@@ -30,16 +60,49 @@ const Timer = props => {
 
   const formatNumber = number => `0${number}`.slice(-2);
 
+  let current, next;
+  if (stage === 1) {
+    current = <Text style={[styles.current, {color: '#fff'}]}>READY!</Text>;
+    next = <Text style={[styles.next, {color: '#00ff00'}]}>NEXT: HANG</Text>;
+  } else if (stage % 2 === 0) {
+    current = <Text style={[styles.current, {color: '#00ff00'}]} >HANG</Text>;
+    next = <Text style={[styles.next, {color: '#ffff00'}]}>NEXT: REST</Text>;
+  } else {
+    current = <Text style={[styles.current, {color: '#ffff00'}]}>REST</Text>;
+    next = <Text style={[styles.next, {color: '#00ff00'}]} >NEXT: HANG</Text>;
+  }
+
   return (
     <View style={styles.container}>
       {/* <StatusBar barStyle='light-content' /> */}
       <Text style={styles.timerText}>{formatNumber(remaining)}</Text>
-      <TouchableOpacity onPress={startStop} style={styles.button}>
-        <Text style={styles.buttonText}>{active ? 'Pause' : 'Start'}</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={reset} style={[styles.button, styles.resetButton]}>
-        <Text style={[styles.buttonText, styles.resetText]}>Reset</Text>
-      </TouchableOpacity>
+      
+      <View style={styles.eir}>
+        <View style={styles.eirInner}>
+          <Text style={styles.eirText}>ELAPSED</Text>
+          <Text style={styles.eirCountText}>{formatNumber(elapsed)}</Text>
+        </View>
+        <View style={styles.eirInner}>
+          <Text style={styles.eirText}>INTERVAL</Text>
+          <Text style={styles.eirCountText}>{`${int}/6`}</Text>
+        </View>
+        <View style={styles.eirInner}>
+          <Text style={styles.eirText}>REMAINING</Text>
+          <Text style={styles.eirCountText}>{formatNumber(remain)}</Text>
+        </View>
+      </View>
+      
+      {current}
+      {next}
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={reset} style={[styles.button, styles.resetButton]}>
+          <Text style={[styles.buttonText, styles.resetText]}>Reset</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={startStop} style={styles.button}>
+          <Text style={styles.buttonText}>{active ? 'Pause' : 'Start'}</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
@@ -56,21 +119,58 @@ const styles = StyleSheet.create({
     fontSize: 90,
     marginBottom: 20
   },
+  eir: {
+    //flex: 1,
+    flexDirection: 'row',
+    width: '90%',
+    justifyContent: 'space-between'
+  },
+  eirInner: {
+    alignItems: 'center',
+    width: '30%'
+  },
+  eirText: {
+    color: '#fff',
+    fontSize: 13
+  },
+  eirCountText: {
+    color: '#fff',
+    fontSize: 23
+  },
+  current: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  next: {
+    fontSize: 20,
+    marginBottom: 20
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'flex-end',
+    marginBottom: 50,
+    justifyContent: 'center'
+  },
   button: {
-    borderWidth: 10,
+    borderWidth: 5,
+    marginLeft: 10,
     borderColor: '#00ff00',
-    width: screen.width / 2,
-    height: screen.width / 2,
-    borderRadius: screen.width / 2,
+    width: screen.width / 4,
+    height: screen.width / 4,
+    borderRadius: screen.width / 4,
     alignItems: 'center',
     justifyContent: 'center'
   },
   buttonText: {
-    fontSize: 45,
+    fontSize: 22,
     color: '#00ff00'
   },
   resetButton: {
-    margin: 20,
+    marginLeft: 0,
+    marginRight: 10,
     borderColor: '#FF851B'
   },
   resetText: {
